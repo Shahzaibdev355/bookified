f2b97ed0aaf8746ede425dbf5cda99b35727197c
Shahzaib <shahzaibsheikh355@gmail.com>
2026-04-02 17:21:26 +0500

    fix error in util file

--- lib/utils.ts
+++ lib/utils.ts
@@ -121,9 +121,15 @@ export async function parsePDFFile(file: File) {
       throw new Error('Could not get canvas context');
     }
 
+    // await firstPage.render({
+    //   canvasContext: context,
+    //   viewport: viewport,
+    // }).promise;
+
     await firstPage.render({
       canvasContext: context,
       viewport: viewport,
+      canvas: canvas
     }).promise;
 
     // Convert canvas to data URL
