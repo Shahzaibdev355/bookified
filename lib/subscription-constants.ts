ff7e694ddc83b1d13b83e3382c01d590c2808e5f
Shahzaib <shahzaibsheikh355@gmail.com>
2026-04-06 03:26:44 +0500

    new change

--- lib/subscription-constants.ts
+++ lib/subscription-constants.ts
@@ -17,7 +17,7 @@ export const PLAN_LIMITS: Record<PlanType, PlanLimits> = {
     [PLANS.FREE]: {
         maxBooks: 6,
         maxSessionsPerMonth: 10,
-        maxDurationPerSession: 3,
+        maxDurationPerSession: 5,
         hasSessionHistory: false,
     },
     [PLANS.STANDARD]: {
